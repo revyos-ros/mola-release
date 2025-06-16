aaab39f101b9e066b0597f6889ead729df7cf8c6
Zheng Junjie <z572@z572.online>
2025-06-16 05:24:20 +0000

    Rebase from 'upstream'

--- mola_launcher/src/MolaDLL_Loader.h
+++ mola_launcher/src/MolaDLL_Loader.h
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 /**
@@ -20,14 +20,13 @@
 /** Used in internal_load_lib_modules() */
 struct LoadedModules
 {
-    std::string lib_path;
-    void*       handle{nullptr};
+  std::string lib_path;
+  void*       handle{nullptr};
 };
 
 /** Loads all libs under lib_search_paths_. \sa setup() */
 void internal_load_lib_modules(
-    mrpt::system::COutputLogger&    app,
-    const std::vector<std::string>& lib_search_paths);
+    mrpt::system::COutputLogger& app, const std::vector<std::string>& lib_search_paths);
 
 /** Returns the current list of loaded module dyanmic libraries. */
 const std::map<std::string, LoadedModules>& get_loaded_modules();
--- mola_traj_tools/src/traj_ypr2tum.cpp
+++ mola_traj_tools/src/traj_ypr2tum.cpp
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 
@@ -11,26 +11,26 @@
 
 int main(int argc, char** argv)
 {
-    try
+  try
+  {
+    if (argc != 3)
     {
-        if (argc != 3)
-        {
-            std::cerr << "Usage: " << argv[0] << " INPUT.ypr  OUTPUT.tum" << std::endl;
-            return 1;
-        }
-
-        const std::string sIn  = argv[1];
-        const std::string sOut = argv[2];
-
-        mrpt::poses::CPose3DInterpolator p;
-        p.loadFromTextFile(sIn);
-        p.saveToTextFile_TUM(sOut);
-
-        return 0;
+      std::cerr << "Usage: " << argv[0] << " INPUT.ypr  OUTPUT.tum" << std::endl;
+      return 1;
     }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-        return 1;
-    }
+
+    const std::string sIn  = argv[1];
+    const std::string sOut = argv[2];
+
+    mrpt::poses::CPose3DInterpolator p;
+    p.loadFromTextFile(sIn);
+    p.saveToTextFile_TUM(sOut);
+
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << std::endl;
+    return 1;
+  }
 }
