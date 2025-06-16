06e9d89c0d9190ff7bd4922dc79f098248f2412a
Zheng Junjie <z572@z572.online>
2025-06-16 05:24:45 +0000

    Rebase from 'release/humble/mola_kernel'

--- src/Factor.cpp
+++ src/Factor.cpp
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 /**
@@ -17,37 +17,37 @@ using namespace mola;
 
 FactorBase& mola::factor_get_base(Factor& f)
 {
-    FactorBase* ret = nullptr;
-    std::visit(
-        overloaded{
-            [&ret](FactorBase& b) { ret = &b; },
-            [&ret](FactorOther& o)
-            {
-                ASSERT_(o);
-                ret = o.get();
-            },
-            [](std::monostate) {}},
-        f);
+  FactorBase* ret = nullptr;
+  std::visit(
+      overloaded{
+          [&ret](FactorBase& b) { ret = &b; },
+          [&ret](FactorOther& o)
+          {
+            ASSERT_(o);
+            ret = o.get();
+          },
+          [](std::monostate) {}},
+      f);
 
-    if (!ret) THROW_EXCEPTION("factor_get_base(): Empty variant.");
+  if (!ret) THROW_EXCEPTION("factor_get_base(): Empty variant.");
 
-    return *ret;
+  return *ret;
 }
 
 const FactorBase& mola::factor_get_base(const Factor& f)
 {
-    const FactorBase* ret = nullptr;
-    std::visit(
-        overloaded{
-            [&ret](const FactorBase& b) { ret = &b; },
-            [&ret](const FactorOther& o)
-            {
-                ASSERT_(o);
-                ret = o.get();
-            },
-            [](std::monostate) {}},
-        f);
+  const FactorBase* ret = nullptr;
+  std::visit(
+      overloaded{
+          [&ret](const FactorBase& b) { ret = &b; },
+          [&ret](const FactorOther& o)
+          {
+            ASSERT_(o);
+            ret = o.get();
+          },
+          [](std::monostate) {}},
+      f);
 
-    if (!ret) THROW_EXCEPTION("factor_get_base(): Empty variant.");
-    return *ret;
+  if (!ret) THROW_EXCEPTION("factor_get_base(): Empty variant.");
+  return *ret;
 }
