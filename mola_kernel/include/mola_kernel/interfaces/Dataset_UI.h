ee3f2e840dca835cc0f69028e18a0d55bcab75a3
Zheng Junjie <z572@z572.online>
2025-06-16 05:24:31 +0000

    Rebase from 'upstream'

--- mola_kernel/include/mola_kernel/interfaces/Dataset_UI.h
+++ mola_kernel/include/mola_kernel/interfaces/Dataset_UI.h
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 /**
@@ -19,28 +19,27 @@ namespace mola
  * \ingroup mola_kernel_interfaces_grp */
 class Dataset_UI
 {
-   public:
-    Dataset_UI();
-    virtual ~Dataset_UI();
+ public:
+  Dataset_UI() = default;
 
-    /** @name Virtual interface of Dataset_UI
-     *{ */
+  /** @name Virtual interface of Dataset_UI
+   *{ */
 
-    /** Number of different time steps available to call getObservations() */
-    virtual size_t datasetUI_size() const = 0;
+  /** Number of different time steps available to call getObservations() */
+  virtual size_t datasetUI_size() const = 0;
 
-    /** Returns the latest requested observation, range [0, datasetSize()] */
-    virtual size_t datasetUI_lastQueriedTimestep() const = 0;
+  /** Returns the latest requested observation, range [0, datasetSize()] */
+  virtual size_t datasetUI_lastQueriedTimestep() const = 0;
 
-    virtual double datasetUI_playback_speed() const       = 0;
-    virtual void   datasetUI_playback_speed(double speed) = 0;
+  virtual double datasetUI_playback_speed() const       = 0;
+  virtual void   datasetUI_playback_speed(double speed) = 0;
 
-    virtual bool datasetUI_paused() const      = 0;
-    virtual void datasetUI_paused(bool paused) = 0;
+  virtual bool datasetUI_paused() const      = 0;
+  virtual void datasetUI_paused(bool paused) = 0;
 
-    /** Forces continue replaying in this moment in time */
-    virtual void datasetUI_teleport(size_t timestep) = 0;
-    /** @} */
+  /** Forces continue replaying in this moment in time */
+  virtual void datasetUI_teleport(size_t timestep) = 0;
+  /** @} */
 };
 
 }  // namespace mola
