bb6e398ba12f9404bebc17f1f5c12b6b65ebc394
Zheng Junjie <z572@z572.online>
2025-06-16 05:26:36 +0000

    Rebase from 'debian/humble/mola_kernel'

--- include/mola_kernel/pretty_print_exception.h
+++ include/mola_kernel/pretty_print_exception.h
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 /**
@@ -17,7 +17,6 @@
 namespace mola
 {
 void pretty_print_exception(
-    const std::exception& e, const std::string& headerLine,
-    const bool use_std_cerr = true);
+    const std::exception& e, const std::string& headerLine, const bool use_std_cerr = true);
 
 }  // namespace mola
