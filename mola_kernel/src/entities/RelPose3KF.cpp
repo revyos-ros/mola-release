5e5de10b8849be2bed36e602fefc332e3f93970b
Zheng Junjie <z572@z572.online>
2025-06-18 07:10:04 +0000

    Rebase from 'upstream'

--- mola_kernel/src/entities/RelPose3KF.cpp
+++ mola_kernel/src/entities/RelPose3KF.cpp
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 /**
@@ -22,23 +22,22 @@ IMPLEMENTS_SERIALIZABLE(RelPose3KF, EntityBase, mola);
 uint8_t RelPose3KF::serializeGetVersion() const { return 0; }
 void    RelPose3KF::serializeTo(mrpt::serialization::CArchive& out) const
 {
-    baseSerializeTo(out);
+  baseSerializeTo(out);
 
-    out << relpose_value << raw_observations_;
+  out << relpose_value << raw_observations_;
 }
-void RelPose3KF::serializeFrom(
-    mrpt::serialization::CArchive& in, uint8_t version)
+void RelPose3KF::serializeFrom(mrpt::serialization::CArchive& in, uint8_t version)
 {
-    baseSerializeFrom(in);
+  baseSerializeFrom(in);
 
-    switch (version)
+  switch (version)
+  {
+    case 0:
     {
-        case 0:
-        {
-            in >> relpose_value >> raw_observations_;
-        }
-        break;
-        default:
-            MRPT_THROW_UNKNOWN_SERIALIZATION_VERSION(version);
-    };
+      in >> relpose_value >> raw_observations_;
+    }
+    break;
+    default:
+      MRPT_THROW_UNKNOWN_SERIALIZATION_VERSION(version);
+  };
 }
