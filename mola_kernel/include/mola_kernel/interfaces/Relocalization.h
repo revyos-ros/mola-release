b0b84442ba417a37669b07f0cdde74b39f689da3
Zheng Junjie <z572@z572.online>
2025-06-18 07:10:05 +0000

    Rebase from 'upstream'

--- mola_kernel/include/mola_kernel/interfaces/Relocalization.h
+++ mola_kernel/include/mola_kernel/interfaces/Relocalization.h
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 /**
@@ -20,26 +20,24 @@ namespace mola
  * \ingroup mola_kernel_interfaces_grp */
 class Relocalization
 {
-   public:
-    Relocalization();
-    virtual ~Relocalization();
+ public:
+  Relocalization() = default;
 
-    /** @name Virtual interface of Relocalization
-     *{ */
+  /** @name Virtual interface of Relocalization
+   *{ */
 
-    /** Re-localize near this pose, including uncetainty.
-     *  \param[in] pose The pose, in the local map frame.
-     *  There is no return value from this method.
-     */
-    virtual void relocalize_near_pose_pdf(
-        const mrpt::poses::CPose3DPDFGaussian& p) = 0;
+  /** Re-localize near this pose, including uncertainty.
+   *  \param[in] pose The pose, in the local map frame.
+   *  There is no return value from this method.
+   */
+  virtual void relocalize_near_pose_pdf(const mrpt::poses::CPose3DPDFGaussian& p) = 0;
 
-    /** Re-localize with the next incoming GNSS message.
-     *  There is no return value from this method.
-     */
-    virtual void relocalize_from_gnss() = 0;
+  /** Re-localize with the next incoming GNSS message.
+   *  There is no return value from this method.
+   */
+  virtual void relocalize_from_gnss() = 0;
 
-    /** @} */
+  /** @} */
 };
 
 }  // namespace mola
--- mola_kernel/src/interfaces/BackEndBase.cpp
+++ mola_kernel/src/interfaces/BackEndBase.cpp
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * See LICENSE for license information.
  * ------------------------------------------------------------------------- */
 /**
@@ -24,22 +24,20 @@ BackEndBase::BackEndBase() = default;
 
 void BackEndBase::initialize(const Yaml& cfg)
 {
-    MRPT_TRY_START
+  MRPT_TRY_START
 
-    // attach to world model:
-    auto wms = findService<WorldModel>();
-    ASSERTMSG_(!wms.empty(), "No WorldModel found in the system!");
-    ASSERTMSG_(
-        wms.size() == 1, "Only one WorldModel can coexist in the system!");
+  // attach to world model:
+  auto wms = findService<WorldModel>();
+  ASSERTMSG_(!wms.empty(), "No WorldModel found in the system!");
+  ASSERTMSG_(wms.size() == 1, "Only one WorldModel can coexist in the system!");
 
-    worldmodel_ = std::dynamic_pointer_cast<WorldModel>(wms[0]);
-    ASSERT_(worldmodel_);
-    MRPT_LOG_INFO_FMT(
-        "Attached to WorldModel module `%s`",
-        worldmodel_->getModuleInstanceName().c_str());
+  worldmodel_ = std::dynamic_pointer_cast<WorldModel>(wms[0]);
+  ASSERT_(worldmodel_);
+  MRPT_LOG_INFO_FMT(
+      "Attached to WorldModel module `%s`", worldmodel_->getModuleInstanceName().c_str());
 
-    // children config:
-    this->initialize_backend(cfg);
+  // children config:
+  this->initialize_backend(cfg);
 
-    MRPT_TRY_END
+  MRPT_TRY_END
 }
