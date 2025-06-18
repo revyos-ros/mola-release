7e191aa55e55f54258bb9b1dc4e8c272cab45f79
Zheng Junjie <z572@z572.online>
2025-06-18 07:09:55 +0000

    Rebase from 'upstream'

--- mola_metric_maps/tests/test-occgrid.cpp
+++ mola_metric_maps/tests/test-occgrid.cpp
@@ -1,7 +1,7 @@
 /* -------------------------------------------------------------------------
  *   A Modular Optimization framework for Localization and mApping  (MOLA)
  *
- * Copyright (C) 2018-2024 Jose Luis Blanco, University of Almeria
+ * Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria
  * Licensed under the GNU GPL v3 for non-commercial applications.
  *
  * This file is part of MOLA.
@@ -29,13 +29,13 @@
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
-    try
-    {
-        // xx();
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << "\n";
-        return 1;
-    }
+  try
+  {
+    // xx();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
 }
